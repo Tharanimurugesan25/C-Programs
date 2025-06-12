9735d9c9c584c47bcdcd567dbbb420a0e94e56a5
Tharanimurugesan25 <tharu4291@gmail.com>
2025-06-12 05:55:21 +0530

    Add prgm1.c program

---
+++ prgm2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+int a,b,sum;
+printf("Enter the value of a,b:");
+scanf("%d" "%d",&a,&b);
+sum = a+b;
+printf("The value of sum is %d",sum);
+return 0 ;
+}
---
+++ prgm3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+char a;
+printf("Enter the value of a:");
+scanf("%c",&a);
+printf("The ASIC values of %d is",a);
+return 0;
+}
